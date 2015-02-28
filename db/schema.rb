e840a7798a3b003b6ea588bc86b7beba189862d8
Nico Moreno <nicom55@gmail.com>
2015-02-28 13:42:58 +0000

    create item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228133532) do
+ActiveRecord::Schema.define(version: 20150228134114) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
