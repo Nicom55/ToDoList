6d79f117ecfd0ff5b570c70413b6c94e6892ff60
Nico Moreno <nicom55@gmail.com>
2015-03-02 12:30:02 +0000

    changing items#create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,13 @@ class ItemsController < ApplicationController
   def create
     @list = List.find(params[:id])
     @item = @list.items.create(item_params)
-    redirect_to list_path(@list)
+    render @item
   end
   
   def update
     @list = List.find(params[:id])
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      #not sure about this since I want to use AJAX and stay on one page
       redirect_to list_path(@list)
     else
       @errors = @item.errors
