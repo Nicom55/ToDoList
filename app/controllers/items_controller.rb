affad59bde04deb8fb0ff969d03b6222241c0ab5
Nico Moreno <nicom55@gmail.com>
2015-03-02 03:38:25 +0000

    worked on items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,17 @@
 class ItemsController < ApplicationController
 
   def create
-    @list = List.find(params[:list_id])
-    @item = @list.item.create(item_params)
-    render @item
+    @list = List.find(params[:id])
+    @item = @list.items.create(item_params)
+    redirect_to list_path(@list)
   end
   
   def update
-    @list = List.find(params[:list_id])
+    @list = List.find(params[:id])
     @item = Item.find(params[:id])
     if @item.update(item_params)
       #not sure about this since I want to use AJAX and stay on one page
-      render item_path
+      redirect_to list_path(@list)
     else
       @errors = @item.errors
       render :nothing => true
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    @list = List.find(params[:list_id])
+    @list = List.find(params[:id])
     @item = Item.find(params[:id])
     @item.destroy
     render :nothing => true
